a99c2b8e35d1487f65e48287881d6a3ed37b8fcc
Robert Soriano <sorianorobertc@gmail.com>
2020-03-02 08:50:54 +0800

    [refactor] Stick repo stats to bottom of card

--- src/components/Repo.svelte
+++ src/components/Repo.svelte
@@ -14,6 +14,19 @@
   }
 </script>
 
+<style>
+  .card {
+    position: relative;
+    padding-bottom: 45px;
+  }
+
+  .repo-stats {
+    position: absolute;
+    bottom: 0px;
+    height: 45px;
+  }
+</style>
+
 <div
   class="card is-full-width has-cursor-pointer"
   on:click|preventDefault={() => handleClick(htmlUrl)}>
@@ -24,28 +37,30 @@
       {#if description}
         <p>{description}</p>
       {/if}
-      <div class="is-pulled-left is-size-7">
-        <span
-          class="icon is-inline-block is-small"
-          style="color: {languageColor};">
-          <i class="fa fa-circle" />
-        </span>
-        <span class="has-margin-right-10">
-          {#if language}{language}{:else}Unknown{/if}
-        </span>
-        <span class="icon is-inline-block is-small">
-          <i class="fa fa-star" />
-        </span>
-        <span class="has-margin-right-10">{stargazersCount}</span>
-        <span class="icon is-inline-block is-small">
-          <i class="fa fa-code-branch" />
-        </span>
-        <span>{forksCount}</span>
+      <div class="repo-stats">
+        <div class="is-pulled-left is-size-7">
+          <span
+            class="icon is-inline-block is-small"
+            style="color: {languageColor};">
+            <i class="fa fa-circle" />
+          </span>
+          <span class="has-margin-right-10">
+            {#if language}{language}{:else}Unknown{/if}
+          </span>
+          <span class="icon is-inline-block is-small">
+            <i class="fa fa-star" />
+          </span>
+          <span class="has-margin-right-10">{stargazersCount}</span>
+          <span class="icon is-inline-block is-small">
+            <i class="fa fa-code-branch" />
+          </span>
+          <span>{forksCount}</span>
+        </div>
+        <div class="is-pulled-right is-size-7">
+          <span>{size} KB</span>
+        </div>
+        <div class="is-clearfix" />
       </div>
-      <div class="is-pulled-right is-size-7">
-        <span>{size} KB</span>
-      </div>
-      <div class="is-clearfix" />
     </div>
   </div>
 </div>
